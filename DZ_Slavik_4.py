a0479b2d9d150cb47eb22183ac3f1125606895d6
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2023-04-14 19:42:16 +0300

    Add files via upload

---
+++ DZ_Slavik_4.py
@@ -0,0 +1,50 @@
+message = input("ведите сообщение: ")
+length = len(message)
+cipher = f"\033[34m\033[43m{message}\033[0m"
+print(cipher)
+print(length) 
+print(f"""Первая буква слова:{message[0]}
+Последние буква слове:{message[-1]}
+Первые три симлова строки:{message[:3]}
+Последни три симлова строки: {message[-3:]}""")
+first_shifr = message.replace('а','т')
+first_shifr = first_shifr.replace('в','е')
+first_shifr = first_shifr.replace('т','р')
+first_shifr = first_shifr.replace('к','т')
+first_shifr = first_shifr.replace('о','х')
+print(f"Первый шифр-{first_shifr}")
+second_shifr = message[::-1]
+print(f"Второй шифр-{second_shifr}")
+third_shifr = message[::2]+message[1::2]
+print(f"третий шифр-{third_shifr}")
+first_letter = message[0]
+last_letter = message[-1]
+quarter_shrift = last_letter + message[1:length-1] + first_letter
+print(f"четвёртый шрифт-{quarter_shrift}")
+half_length = length//2
+fifth_shrift = message[half_length:] + message[:half_length]
+print(f"пятый шрифт-{fifth_shrift}")
+rash_shrift_one = first_shifr.replace('т','а')
+rash_shrift_one = rash_shrift_one.replace('е','в')
+rash_shrift_one = rash_shrift_one.replace('р','т')
+rash_shrift_one = rash_shrift_one.replace('т','к')
+rash_shrift_one = rash_shrift_one.replace('х','о')
+print(f"расшифровока 1 шрифта-{rash_shrift_one}")
+rash_shrift_two = second_shifr[::-1]
+print(f"расшифровока 2 шрифта-{rash_shrift_two}")
+rash_shrift_three = ""
+if length%2 == 1:
+    half_length+=1
+tmp_string1 = third_shifr [:half_length]
+tmp_string2 = third_shifr [half_length:]
+for symbol in range(len(tmp_string1)):
+    rash_shrift_three += tmp_string1[symbol]
+    if symbol < len(tmp_string2):
+        rash_shrift_three += tmp_string2[symbol]
+print(f"расшифровока 3 шрифта-{rash_shrift_three}")
+first_letter = quarter_shrift[0]
+last_letter = quarter_shrift[-1]
+rash_shrift_four = last_letter + quarter_shrift[1:length-1] + first_letter
+print(f"расшифровока 4 шрифта-{rash_shrift_four}")
+rash_shrift_five = fifth_shrift[half_length:] + fifth_shrift[:half_length]
+print(f"расшифровока 5 шрифта-{rash_shrift_five}")
